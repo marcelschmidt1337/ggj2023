004cd1d07a55d94bbf18f7dce6d30abebd02ea91
Marcel Schmidt <github@m-schmidt.org>
2023-02-04 14:32:52 +0100

    Added basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public event Action OnPickedUp;
+
+    [SerializeField] private InputAction moveAction;
+    [SerializeField] private InputAction pickupAction;
+    [SerializeField] private InputAction throwAction;
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private float speedMultiplier = 1f;
+
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        pickupAction.Enable();
+        throwAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        pickupAction.Disable();
+        throwAction.Disable();
+    }
+
+    private void Awake()
+    {
+        pickupAction.performed += OnPickup;
+        throwAction.performed += OnThrow;
+    }
+
+    private void Update()
+    {
+        var moveAmount = moveAction.ReadValue<Vector2>();
+        rigidbody.velocity += moveAmount * (Time.fixedDeltaTime * speedMultiplier);
+    }
+}
