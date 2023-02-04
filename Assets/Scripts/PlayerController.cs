6f1e34071f68d881ef6a2a5b10ef4b6b3d1063c2
Marcel Schmidt <github@m-schmidt.org>
2023-02-04 19:43:22 +0100

    Merged throw and pickup methods

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,38 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerState playerState;
+    [SerializeField] private PlayerInput playerInput;
     [SerializeField] private PickupTargetSensor pickupTargetSensor;
     [SerializeField] private new Rigidbody2D rigidbody;
     [SerializeField] private float speedMultiplier = 1f;
 
     private Vector2 moveDir = Vector2.zero;
 
+    private void OnEnable()
+    {
+        playerInput.onActionTriggered += EventHandler;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.onActionTriggered -= EventHandler;
+    }
+
+    private void EventHandler(InputAction.CallbackContext context)
+    {
+        switch (context.action.name)
+        {
+            case "move":
+                OnMove(context);
+                break;
+            case "pickup":
+                OnPickup(context);
+                break;
+            case "throw":
+                OnThrow(context);
+                break;
+        }
+    }
 
     private void Update()
     {
@@ -25,67 +51,84 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void OnMove(InputValue value)
+    private void OnMove(InputAction.CallbackContext context)
     {
-        moveDir = value.Get<Vector2>();
+        moveDir = context.ReadValue<Vector2>();
     }
 
-    #region Pickup
-
-    private void OnStartPickup(InputAction.CallbackContext obj)
+    private void OnPickup(InputAction.CallbackContext context)
     {
-        if (playerState.CanPickUp && pickupTargetSensor.HasPickupTarget)
+        switch (context.phase)
         {
-            playerState.CurrentAction = PlayerAction.PickingUp;
+            case InputActionPhase.Started:
+            {
+                if (playerState.CanPickUp && pickupTargetSensor.HasPickupTarget)
+                {
+                    playerState.CurrentAction = PlayerAction.PickingUp;
+                }
+
+                break;
+            }
+            case InputActionPhase.Canceled:
+            {
+                if (playerState.CurrentAction == PlayerAction.PickingUp)
+                {
+                    playerState.CurrentAction = PlayerAction.None;
+                }
+
+                break;
+            }
+            case InputActionPhase.Performed:
+            {
+                if (!playerState.CanPickUp || !pickupTargetSensor.HasPickupTarget)
+                {
+                    return;
+                }
+
+                playerState.CurrentAction = PlayerAction.Carrying;
+
+                //Pair carrot to player
+                var target = pickupTargetSensor.CurrentPickupTarget.transform;
+                target.SetParent(transform);
+                target.localPosition = new Vector2(0, 0.5f);
+                Debug.Log($"Picking up: {target}");
+                playerState.ObjectCarrying = target;
+                break;
+            }
         }
     }
 
-    private void OnCancelPickup(InputAction.CallbackContext obj)
+    private void OnThrow(InputAction.CallbackContext context)
     {
-        if (playerState.CurrentAction == PlayerAction.PickingUp)
+        switch (context.phase)
         {
-            playerState.CurrentAction = PlayerAction.None;
+            case InputActionPhase.Started:
+            {
+                if (playerState.CurrentAction != PlayerAction.Carrying || playerState.ObjectCarrying == null)
+                {
+                    return;
+                }
+
+                Debug.Log($"Start throwing up: {playerState.ObjectCarrying}");
+
+                playerState.CurrentAction = PlayerAction.Throwing;
+                break;
+            }
+            case InputActionPhase.Performed:
+            {
+                if (playerState.CurrentAction != PlayerAction.Throwing || playerState.ObjectCarrying == null)
+                {
+                    return;
+                }
+
+                Debug.Log($"Performing throw: {playerState.ObjectCarrying}");
+
+                //Should execute throw, but drop carrot for now
+                playerState.ObjectCarrying.SetParent(null);
+                playerState.ObjectCarrying = null;
+                playerState.CurrentAction = PlayerAction.None;
+                break;
+            }
         }
-    }
-
-    private void OnPerformedPickup(InputAction.CallbackContext obj)
-    {
-        if (!playerState.CanPickUp || !pickupTargetSensor.HasPickupTarget)
-        {
-            return;
-        }
-
-        playerState.CurrentAction = PlayerAction.Carrying;
-
-        //Pair carrot to player
-        var target = pickupTargetSensor.CurrentPickupTarget.transform;
-        target.SetParent(transform);
-        target.localPosition = new Vector2(0, 0.5f);
-        Debug.Log($"Picking up: {target}");
-        playerState.ObjectCarrying = target;
-    }
-
-    #endregion
-
-    private void OnStartThrow(InputAction.CallbackContext obj)
-    {
-        if (playerState.CurrentAction != PlayerAction.Carrying || playerState.ObjectCarrying == null) return;
-
-        Debug.Log($"Start throwing up: {playerState.ObjectCarrying}");
-
-        playerState.CurrentAction = PlayerAction.Throwing;
-    }
-
-
-    private void OnPerformedThrow(InputAction.CallbackContext obj)
-    {
-        if (playerState.CurrentAction != PlayerAction.Throwing || playerState.ObjectCarrying == null) return;
-
-        Debug.Log($"Performing throw: {playerState.ObjectCarrying}");
-
-        //Should execute throw, but drop carrot for now
-        playerState.ObjectCarrying.SetParent(null);
-        playerState.ObjectCarrying = null;
-        playerState.CurrentAction = PlayerAction.None;
     }
 }
