3460b7862d5873a2f74df40daed51e0b2e9151db
Ludwig Lauer <ludwig@tenjin.com>
2023-02-04 20:41:38 +0100

    More Animation Controller

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -6,8 +6,21 @@ public class PlayerAnimationController : MonoBehaviour
     [SerializeField] private PlayerState playerState;
     [SerializeField] private Animator animator;
     
-    private static readonly int Walking = Animator.StringToHash("Walking");
+    private static readonly int Idle = Animator.StringToHash("Idle");
+    private static readonly int Up = Animator.StringToHash("Up");
+    private static readonly int Down = Animator.StringToHash("Down");
+    private static readonly int Left = Animator.StringToHash("Left");
+    private static readonly int Right = Animator.StringToHash("Right");
 
+    private static readonly string[] Actions =
+    {
+        "NoneAction",
+        "PickingUpAction",
+        "CarryingAction",
+        "ThrowingAction",
+        "StunnedAction",
+    };
+    
     private void Awake()
     {
         playerState.OnActionChanged += OnPlayerActionChanged;
@@ -15,11 +28,23 @@ public class PlayerAnimationController : MonoBehaviour
 
     private void LateUpdate()
     {
-        animator.SetBool(Walking, playerState.IsWalking);
+        UpdateWalking();
+    }
+
+    private void UpdateWalking()
+    {
+        animator.SetBool(Idle, playerState.IsWalking);
+        animator.SetBool(Up, playerState.WalkDirection.y > 0);
+        animator.SetBool(Down, playerState.WalkDirection.y < 0);
+        animator.SetBool(Left, playerState.WalkDirection.x > 0);
+        animator.SetBool(Right, playerState.WalkDirection.x < 0);
     }
 
     private void OnPlayerActionChanged(PlayerAction action)
     {
-        animator.SetTrigger(action.ToString());
+        foreach (var actionStr in Actions)
+        {
+            animator.SetBool(actionStr,$"{action}+Action" == actionStr);
+        }
     }
 }
