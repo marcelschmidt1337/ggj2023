5d069bd253b9179bc4757f7e9c4f81108772f0d8
Ludwig Lauer <ludwig@tenjin.com>
2023-02-04 15:47:26 +0100

    Add PickupTargetSensor

---
+++ Assets/PickupTargetSensor.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PickupTargetSensor : MonoBehaviour
+{
+    public Collider2D CurrentPickupTarget { get; private set; }
+    
+    public List<Collider2D> targetsInRange = new();
+
+    protected void OnValidate()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (targetsInRange.Contains(other)) return;
+        
+        targetsInRange.Add(other);
+        
+        UpdateCurrentInteractable();
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D other)
+    {
+        if (targetsInRange.Contains(other))
+        {
+            targetsInRange.Remove(other);
+        }
+        
+        UpdateCurrentInteractable();
+    }
+
+    private void UpdateCurrentInteractable()
+    {
+        CurrentPickupTarget = targetsInRange.Count > 0 ? targetsInRange[^1] : null;
+   
+        Debug.Log($"Current pickup target: {CurrentPickupTarget}");
+    }
+}
