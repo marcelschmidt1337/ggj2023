c73b9b66fb3a6087be00778e1ec0c0a5b1041172
Ludwig Lauer <ludwig@tenjin.com>
2023-02-05 14:39:23 +0100

    Working UI with controller nav

--- Assets/Scripts/HowToController.cs
+++ Assets/Scripts/HowToController.cs
@@ -1,9 +1,17 @@
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HowToController : MonoBehaviour
 {
+    [SerializeField] private Button backButton;
+
+    private void Start()
+    {
+        backButton.Select();
+    }
+    
     [UsedImplicitly]
     public void OnReturnToMainMenuClicked()
     {
