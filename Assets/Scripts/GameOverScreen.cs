12b1eb7d5fbbbfc6823864d8cf8f8fbb079866cb
Ludwig Lauer <ludwig@tenjin.com>
2023-02-11 19:46:51 +0100

    Fix controller navigation in game over screen

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
+    [SerializeField] private Button replayButton;
     [SerializeField] private Animator animator;
-    
+
     private static readonly int ShowHash = Animator.StringToHash("Show");
     private static readonly int Player1Won = Animator.StringToHash("Player1Won");
     private static readonly int Player2Won = Animator.StringToHash("Player2Won");
@@ -18,6 +20,8 @@ public class GameOverScreen : MonoBehaviour
         animator.SetBool(Player1Won, result == GameResult.Player1Wins);
         animator.SetBool(Player2Won, result == GameResult.Player2Wins);
         animator.SetBool(Draw, result == GameResult.Draw);
+
+        replayButton.Select();
     }
 
     public void Replay()
